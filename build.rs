0cbab181728774602079c7e91e507bccb571fb28
Lilian A. Moraru <lilian.moraru90@gmail.com>
2017-04-09 04:08:47 +0300

    Add QtCreator build

--- build.rs
+++ build.rs
@@ -1,5 +1,6 @@
 extern crate cmake;
 extern crate dotenv;
+extern crate num_cpus;
 
 use std::process::Command;
 use std::path::{ Path, PathBuf };
@@ -49,35 +50,71 @@ fn main() {
 
     let deps_folder = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("deps");
     // Making sure that we clone the submodules
-    {
-        let mut should_update = false;
-        for path in &["clang", "cxxbasics", "llvm", "qt-creator"] {
-            if !deps_folder.join(path).join(".git").exists() {
-                should_update = true;
-            }
-        }
-
-        if should_update {
-            let mut git_submodule_update = Command::new("git");
-            run(git_submodule_update.args(&["submodule", "update", "--init", "--recursive"]),
-                "git");
-        }
-    }
+    git_submodule_update(&deps_folder);
 
     // Compiling Clang and QtCreator
-    add_clang_link(deps_folder.as_path());
-    compile_clang(deps_folder.as_path());
-    compile_qtcreator(deps_folder.as_path());
+    add_clang_link(&deps_folder);
+    compile_clang(&deps_folder);
+    compile_qtcreator(&deps_folder, &qmake_path);
 }
 
-fn compile_qtcreator(deps_folder: &Path) {
-    // CMAKE_CXX_COMPILER = deps/clang-toolchain/bin/clang++
-    // -C deps/cxxbasics/CXXBasics.cmake
+fn compile_qtcreator(deps_folder: &Path, qmake_path: &String) {
+    // If the "qtcreator" binary already exists, don't try to build
+    if deps_folder.join("qt-creator").join("build")
+                  .join("bin").join("qtcreator").exists() {
+        return;
+    }
+
+    // Lets also build Clang Code Model
+    ::std::env::set_var("LLVM_INSTALL_DIR", deps_folder.join("clang-toolchain"));
+
+    // Adding the newly compiled toolchain to the "PATH" environment variable
+    {
+        let clang_bin_path = deps_folder.join("clang-toolchain").join("bin");
+        let mut path_final = String::from(clang_bin_path.to_str().unwrap());
+        path_final.push(':');
+        path_final.push_str(env!("PATH"));
+
+        ::std::env::set_var("PATH", path_final);
+    }
+
+    let qtcreator_path  = deps_folder.join("qt-creator");
+    let qtcreator_build = qtcreator_path.join("build");
+    let qtcreator_src = qtcreator_path.to_str().unwrap();
+
+    // Create build directory
+    ::std::fs::create_dir_all(qtcreator_build.as_path()).unwrap();
+
+    // Running "qmake CONFIG+=[PROFILE] -r"
+    let mut qmake_command = Command::new(qmake_path.as_str());
+    qmake_command.current_dir(qtcreator_build.as_path());
+
+    let config = {
+        let mut config = String::from("CONFIG+=");
+        let profile = ::std::env::var("PROFILE").unwrap();
+        config.push_str(profile.as_str());
+
+        config
+    };
+
+    if cfg!(target_os = "linux") {
+        qmake_command.args(&[&config, "-spec", "linux-clang", "-r", qtcreator_src]);
+    } else if cfg!(macos) {
+        qmake_command.args(&[&config, "-spec", "macx-clang", "-r", qtcreator_src]);
+    } else {
+        qmake_command.args(&[&config, "-r", qtcreator_src]);
+    }
+    run(&mut qmake_command, "qmake");
+
+    // Lets fire it up( make -j $(nproc) )
+    let cpus = ::num_cpus::get().to_string();
+    let mut make_command = Command::new("make");
+    make_command.current_dir(qtcreator_build.as_path());
+    make_command.args(&["-j", &cpus]);
+    run(&mut make_command, "make");
 }
 
 fn compile_clang(deps_folder: &Path) {
-    use std::ffi::OsString;
-
     // Can't use CXXBasics, limitation in the `cmake` crate
     //    let cxxbasics_path = deps_folder.join("cxxbasics/CXXBasics.cmake");
     let install_path = deps_folder.join("clang-toolchain");
@@ -86,7 +123,6 @@ fn compile_clang(deps_folder: &Path) {
     }
 
     let llvm_src_path = deps_folder.join("llvm");
-    let build_path    = deps_folder.join("build-clang");
 
     // Select a generator
     let mut cmake_config = Config::new(llvm_src_path);
@@ -123,6 +159,21 @@ fn add_clang_link(deps_folder: &Path) {
             use std::os::windows::fs::symlink_dir;
             symlink_dir(deps_folder.join("clang"), expected_clang_path).unwrap();
         }
+    }
+}
+
+fn git_submodule_update(deps_folder: &Path) {
+    let mut should_update = false;
+    for path in &["clang", "cxxbasics", "llvm", "qt-creator"] {
+        if !deps_folder.join(path).join(".git").exists() {
+            should_update = true;
+        }
+    }
+
+    if should_update {
+        let mut git_submodule_update = Command::new("git");
+        run(git_submodule_update.args(&["submodule", "update", "--init", "--recursive"]),
+            "git");
     }
 }
 
